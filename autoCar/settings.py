8e3bf61c8ba0750d26201d02ac448aca46a4e9a2
Farouk <faroukmethkal@gmail.com>
2019-04-21 20:18:39 +0200

    aaa

--- autoCar/settings.py
+++ autoCar/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = [
  os.path.join(BASE_DIR, 'autoCar/static/')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
+# 'static_cdn', 
 django_heroku.settings(locals())
