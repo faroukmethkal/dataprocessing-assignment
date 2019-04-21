6302cbb9ef7058b8899cfc2956f2a72f61d9191b
Farouk <faroukmethkal@gmail.com>
2019-04-21 21:08:09 +0200

    bb

--- autoCar/settings.py
+++ autoCar/settings.py
@@ -136,6 +136,6 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'autoCar/static'),
 )
 django_heroku.settings(locals())
