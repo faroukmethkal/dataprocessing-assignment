138b9325a7daf6aabfe25158294c8c002b1fe7b2
Farouk <faroukmethkal@gmail.com>
2019-04-21 21:06:18 +0200

    bbbbb

--- autoCar/settings.py
+++ autoCar/settings.py
@@ -118,12 +118,24 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATICFILES_DIRS = [
- os.path.join(BASE_DIR, 'autoCar/static/')
-]
+# # https://docs.djangoproject.com/en/2.1/howto/static-files/
+# STATICFILES_DIRS = [
+#  os.path.join(BASE_DIR, 'autoCar/static/')
+# ]
+#
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+# # 'static_cdn',
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-# 'static_cdn',
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 django_heroku.settings(locals())
